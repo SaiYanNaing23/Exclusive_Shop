ca570cdc3209350523b3de9c2642490ae63ed05c
Saiyannaing23 <pesilay2372002@gmail.com>
2023-08-03 02:07:26 +0630

    Header and Nav

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid header-wrapper ">
+    <div class="text-center">
+        <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
+        <a href="#" class="text-white">ShopNow</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "HeaderTop"
+}
+</script>
+
+<style>
+.header-wrapper{
+    padding-top: .8rem;
+    background-color: #000;
+}
+.header-wrapper div p {
+    color: #fff;
+    display: inline-block;
+    margin-right: 5px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light border">
+  <div class="container-fluid mt-2">
+        <a class="navbar-brand fw-bold ms-5 fs-4" href="#">Exclusive</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-auto fs-5">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sign Up</a>
+        </li>
+      </ul>
+          <div class="me-5">
+        <a href="#"><ion-icon name="heart-outline" size="small" class="text-black d-{inline-block } mx-3"></ion-icon></a>
+        <a href="#"><ion-icon name="cart-outline" size="small" class="text-black"></ion-icon></a>
+    </div>
+    </div>
+
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name : "NavBar",
+}
+</script>
+
+<style>
+.navbar{
+    background-color: #fff !important;
+}
+</style>
